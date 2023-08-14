8b8f4fb6f5266270d19e795d9a56648784089b8b
AaryaGhate <136184830+AaryaGhate@users.noreply.github.com>
2023-08-14 18:29:16 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,15 +35,20 @@ def main():
     
     # User input
     user_id = st.number_input("Enter User ID", min_value=1, max_value=1000)
-    product_name = "tshirt"  # For t-shirt recommendations
-    category = "jeans"       # For jeans recommendations
+    product_name = st.selectbox("Select Product Name", data['Product Name'].unique())
+    category = st.selectbox("Select Category", data['Category'].unique())
     
     # Recommendation button
     if st.button("Get Recommendations"):
         recommendations = get_recommendations(user_id, product_name, category, interaction_matrix, product_similarity)
         
+        if len(recommendations) > 10:
+            random_recommendations = random.sample(list(recommendations['Product ID']), 10)
+        else:
+            random_recommendations = list(recommendations['Product ID'])
+        
         # Display recommended products in tabular format
-        recommended_products_info = recommendations[['Product ID', 'Product Name', 'Category', 'Brand']].head(5)
+        recommended_products_info = data[data['Product ID'].isin(random_recommendations)][['Product ID', 'Product Name', 'Category', 'Brand']]
         st.write("Recommended Products:")
         st.table(recommended_products_info)
 

402d018e6d97d761ae53bfdfeb911d8e67c600e2
AaryaGhate <136184830+AaryaGhate@users.noreply.github.com>
2023-08-14 15:56:02 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,78 +9,38 @@ data = pd.read_csv("fashion_products.csv")
 interaction_matrix = data.pivot_table(index='User ID', columns='Product ID', values='Rating', fill_value=0)
 product_similarity = cosine_similarity(interaction_matrix.T)
 
-# Function to get user's purchased products
-def get_user_purchases(user_id):
-    user_purchases = data[data['User ID'] == user_id]
-    return user_purchases['Product ID'].tolist()
-
-# Function to filter recommended products based on rules
-def filter_recommended_products(recommended_products, user_id):
-    user_purchases = get_user_purchases(user_id)
-    filtered_products = []
-    
-    for product_id in recommended_products:
-        product_row = data[data['Product ID'] == product_id].iloc[0]
-        category = product_row['Category']
-        size = product_row['Size']
-        product_name = product_row['Product Name']
-        
-        if product_id not in user_purchases and (category, size) in user_purchases:
-            if product_name == 'Dress':
-                user_dress = user_purchases[(user_purchases['Product Name'] == 'Dress') & ((user_purchases['Brand'] == product_row['Brand']) | (user_purchases['Color'] == product_row['Color']))]
-                if not user_dress.empty:
-                    filtered_products.append(product_id)
-
-            elif product_name == 'Shoes':
-                user_shoes = user_purchases[(user_purchases['Product Name'] == 'Shoes') & ((user_purchases['Brand'] == product_row['Brand']) | (user_purchases['Color'] == product_row['Color']))]
-                if not user_shoes.empty:
-                    filtered_products.append(product_id)
-
-            elif product_name == 'Tshirt':
-                user_tshirt = user_purchases[(user_purchases['Product Name'] == 'Tshirt') & (user_purchases['Brand'] == product_row['Brand'])]
-                if not user_tshirt.empty:
-                    filtered_products.append(product_id)
-                    
-            elif product_name == 'Jeans':
-                user_jeans = user_purchases[(user_purchases['Product Name'] == 'Jeans') & ((user_purchases['Color'] != product_row['Color']) | (user_purchases['Brand'] == product_row['Brand']))]
-                if not user_jeans.empty:
-                    filtered_products.append(product_id)
-                    
-            elif product_name == 'Sweater':
-                user_sweater = user_purchases[(user_purchases['Product Name'] == 'Sweater') & ((user_purchases['Color'] == product_row['Color']) | (user_purchases['Brand'] == product_row['Brand']))]
-                if not user_sweater.empty:
-                    filtered_products.append(product_id)
-    
-    return filtered_products
-
-# Function to get product recommendations based on user interactions and rules
-def get_recommendations(user_id, interaction_matrix, product_similarity, num_recommendations=10):
+# Function to get product recommendations based on category and size
+def get_recommendations(user_id, category, size, interaction_matrix, product_similarity, num_recommendations=5):
     user_interactions = interaction_matrix.loc[user_id].values
     similar_scores = product_similarity.dot(user_interactions)
     recommended_indices = similar_scores.argsort()[-num_recommendations:][::-1]
     recommended_products = interaction_matrix.columns[recommended_indices]
     
-    # Filter recommended products based on rules
-    filtered_products = filter_recommended_products(recommended_products, user_id)
+    # Filter recommended products by category and size
+    filtered_products = filter_by_category_and_size(recommended_products, category, size)
     
     return filtered_products
 
+# Function to filter recommended products by category and size
+def filter_by_category_and_size(products, category, size):
+    return [product_id for product_id in products if data[data['Product ID'] == product_id].iloc[0]['Category'] == category and data[data['Product ID'] == product_id].iloc[0]['Size'] == size]
+
 # Streamlit app
 def main():
-    st.title("Personalized Fashion Product Recommender")
+    st.title("Fashion Product Recommender")
     st.write("Discover personalized fashion product recommendations.")
     
     # User input
     user_id = st.number_input("Enter User ID", min_value=1, max_value=1000)
+    category = st.selectbox("Select Category", data['Category'].unique())
+    size = st.selectbox("Select Size", data['Size'].unique())
     
     # Recommendation button
     if st.button("Get Recommendations"):
-        recommendations = get_recommendations(user_id, interaction_matrix, product_similarity, num_recommendations=10)
+        recommendations = get_recommendations(user_id, category, size, interaction_matrix, product_similarity)
         
-        # Display recommended product IDs in a tabular format
-        recommendations_df = pd.DataFrame(recommendations, columns=["Recommended Product IDs"])
-        st.write("Recommended Products:")
-        st.dataframe(recommendations_df)
+        # Display recommended product IDs
+        st.write("Recommended Product IDs:", recommendations)
 
 if __name__ == "__main__":
     main()
